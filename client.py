f476f31c1aa50e9d8fe9414ea6d7379105728e20
Ash Ahmed <ashlanahmed@college.harvard.edu>
2023-02-15 15:17:39 -0500

    multithreading for geeksforgeeks example working, plus global variable test

--- client.py
+++ client.py
@@ -1,17 +1,37 @@
 # Import socket module
-import socket            
- 
-# Create a socket object
-s = socket.socket()        
- 
-# Define the port on which you want to connect
-port = 49153               
- 
-# connect to the server on local computer
-s.connect(('127.0.0.1', port))
- 
-# receive data from the server and decoding to get the string.
-print (s.recv(1024).decode())
-# close the connection
-s.close()    
-     
+import socket
+
+
+def main():
+	# local host IP '127.0.0.1'
+	host = '127.0.0.1'
+
+	# Define the port on which you want to connect
+	port = 49153
+
+	s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+	# connect to server on local computer
+	s.connect((host,port))
+
+	# message you send to server
+	message = input('\ntype your message lmao :')
+
+	while True:
+
+		# message sent to server
+		s.send(message.encode('ascii'))
+
+		# message received from server
+		data = s.recv(1024)
+
+		# print the received message
+		# here it would be a reverse of sent message
+		print('Received from the server :',str(data.decode('ascii')))
+
+		break
+	# close the connection
+	s.close()
+
+if __name__ == '__main__':
+	main()
--- server.py
+++ server.py
@@ -1,38 +1,72 @@
-# first of all import the socket library
-import socket			
+# import socket programming library
+import socket
 
-# next create a socket object
-s = socket.socket()		
-print ("Socket successfully created")
+# import thread module
+from _thread import *
+import threading
 
-# reserve a port on your computer in our
-# case it is 12345 but it can be anything
-port = 49153			
+print_lock = threading.Lock()
 
-# Next bind to the port
-# we have not typed any ip in the ip field
-# instead we have inputted an empty string
-# this makes the server listen to requests
-# coming from other computers on the network
-s.bind(('', port))		
-print ("socket binded to %s" %(port))
+counter = 0
 
-# put the socket into listening mode
-s.listen(5)	
-print ("socket is listening")		
 
-# a forever loop until we interrupt it or
-# an error occurs
-while True:
-    # Establish connection with client.
-    c, addr = s.accept()	
-    print ('Got connection from', addr )
+# thread function
+def threaded(c):
+	global counter
+	while True:
 
-    # send a thank you message to the client. encoding to send byte type.
-    c.send('BRUHHHHHH'.encode())
+		# data received from client
+		data = c.recv(1024)
+		if not data:
+			print('Bye')
+			# lock released on exit
+			print_lock.release()
+			break
 
-    # Close the connection with the client
-    c.close()
+		msg = data.decode('ascii')
+		msg = msg[::-1]
+		msg += ', ' + str(counter)
 
-    # Breaking once connection closed
-    break
+		# reverse the given string from client
+		data = msg.encode('ascii')
+
+		# send back reversed string to client
+		c.send(data)
+		counter += 1
+
+	# connection closed
+	c.close()
+
+
+def main():
+	host = ""
+
+	# reserve a port on your computer
+	# in our case it is 12345 but it
+	# can be anything
+	port = 49153
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.bind((host, port))
+	print("socket binded to port", port)
+
+	# put the socket into listening mode
+	s.listen(5)
+	print("socket is listening")
+
+	# a forever loop until client wants to exit
+	while True:
+
+		# establish connection with client
+		c, addr = s.accept()
+
+		# lock acquired by client
+		print_lock.acquire()
+		print('Connected to :', addr[0], ':', addr[1])
+
+		# Start a new thread and return its identifier
+		start_new_thread(threaded, (c,))
+	s.close()
+
+
+if __name__ == '__main__':
+	main()
